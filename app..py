b5f83e0a554ac7b465d966070943a3684efacb81
Kimberly Cardenas <you@example.com>
2021-09-02 21:47:38 -0700

    moving on to stations

--- app..py
+++ app..py
@@ -23,88 +23,103 @@ Measurement= Base.classes.measurement
 Station= Base.classes.station
 
 # Create our session 
-session = Session(engine)
+# session = Session(engine)
 
-# # flask
-# app = Flask(__name__)
-
-# hello_dict = {"Hello": "World!"}
-
-# # flask routes
-# @app.route("/")
-# def home():
-#     return (f""
-#     f"/api/v1.0/precipitation"
-#     f"/api/v1.0/stations"
-#     f"/api/v1.0/tobs"
-#     f"/api/v1.0/<start>"
-#     f"/api/v1.0/<start>/<end>")
+# flask
+app = Flask(__name__)
 
 
-# @app.route("/api/v1.0/precipitation")
-# def precipatation():
-#     results=(session.query(Measurement.date, Measurement.prcp)
-#                     .filter(Measurement.date> year_ago).\
-#                     order_by(Measurement.date).all())
-
-#     precip_scores=[]
-#     for results in results:
-#             precip_scores_dict={}
-#             precip_scores_dict["date"]=result.date
-#             precip_scores_dict["prcp"]=result.prcp
-#             precip_scores.append(precip_scores_dict)
-
-#     return jsonify(precip_scores)
+# flask routes
+@app.route("/")
+def home():
+    return (f""
+    f"/api/v1.0/precipitation"
+    f"/api/v1.0/stations"
+    f"/api/v1.0/tobs"
+    f"/api/v1.0/<start>"
+    f"/api/v1.0/<start>/<end>")
 
 
-# @app.route("/api/v1.0/stations")
-# def stations():
-#     results= session.query(Station.station).all()
-    
-#     Stations=[]
-#     for Stations in stations:
-#         station_dict={}
-#         Stations.append(station_dict)
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    year_ago= dt.date(2017,8,23)-dt.timedelta(days=365)
+    session.close()
+    results=(session.query(Measurement.date, Measurement.prcp)
+                    .filter(Measurement.date> year_ago).\
+                    order_by(Measurement.date).all())
 
-#     return jsonify(Stations)
+    precip_scores=[]
+    for results in results:
+            precip_scores_dict={}
+            precip_scores_dict["date"]=results.date
+            precip_scores_dict["prcp"]=results.prcp
+            precip_scores.append(precip_scores_dict)
 
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-#     results= ()
-
-# @app.route("/api/v1.0/<start>")
-# def start():
-#     results=session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
-# active_stations
-
-#     observations=[]
-#     for min,avg,max in results:
-#         start_dict={}
-#         start_dict["Min"]=min
-#         start_dict["Average"]=avg
-#         start_dict["Max"]=max
-#         observations.append(start_dict)
-
-#     return jsonify(observations)
-
-# @app.route("/api/v1.0/<start>/<end>")
-# def first_last():
-#     results=session.query(Measurement.station, Measurement.date, Measurement.tobs).filter(Measurement.date>="2016-08-24").filter(Measurement.date<="2017-08-23").filter(Measurement.station=="USC00519281").all()
+    return jsonify(precip_scores)
 
 
-# Both=[]
-# for min,avg,max in results:
-#     first_last_dict={}
-#     first_last_dict["Min"]=min
-#     first_last_dict["Average"]=avg
-#     first_last_dict["Max"]=max
-#     Both.append(start_dict)
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results= session.query(Station.station).all()
+    session.close()
+    Stations=[]
+    for Stations in stations:
+        station_dict={}
+        Stations.append(station_dict)
 
-# return jsonify(Both)
+    return jsonify(Stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    data=[Measurement.date, Measurement.tobs]
+    results= session.query(*data).filter(Measurement.date >= date).all()
+    session.close()
+    tobs=[]
+    for date, tobs in result:
+        tob_dict={}
+        tob_dict["Date"] = date
+        tob_dict["Temp observations"]= tobs
+        tobs.append(tob_dict)
+    return(tobs)
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+@app.route("/api/v1.0/<start>")
+def start():
+    session = Session(engine)
+    results=session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+    session.close()
+    observations=[]
+    for min,avg,max in results:
+        start_dict={}
+        start_dict["Min"]=min
+        start_dict["Average"]=avg
+        start_dict["Max"]=max
+        observations.append(start_dict)
+
+    return jsonify(observations)
+
+@app.route("/api/v1.0/<start>/<end>")
+def first_last():
+    session = Session(engine)
+    results=session.query(Measurement.station, Measurement.date, Measurement.tobs).filter(Measurement.date>="2016-08-24").filter(Measurement.date<="2017-08-23").filter(Measurement.station=="USC00519281").all()
+    session.close()
+
+    Both=[]
+    for min,avg,max in results: 
+        first_last_dict={}
+        first_last_dict["Min"]=min
+        first_last_dict["Average"]=avg
+        first_last_dict["Max"]=max
+        Both.append(start_dict)
+
+    return jsonify(Both)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 # In[ ]:
